869a289f4abfcfa1029be60bc666391d9e79fb7f
efan andrian <efan.andrian05@gmail.com>
2021-04-26 13:06:36 +0700

    ds first commit

---
+++ DS_C_timer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for (( i=21; i>0; i--)); do
+  sleep 1 &
+  printf "Jangan di tutup, sedang proses update. Trims as@epic  $i \r"
+  wait
+done
+"C:\ds_cibinong\kill_DS.exe" $1 &
+git pull
+"C:\ds_cibinong\DesktopService.exe" $1 &
+
+start chrome http://10.122.250.12/app/Login
+
+
